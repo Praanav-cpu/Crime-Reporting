3cf3b6565a1a97b0ca374c13133b3bc6fb441e00
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-05 13:20:08 +0530

    fatal commits

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// App.jsx
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700"> write your Crime Here</h1>
+    </div>
+  );
+}
+
+export default App;
