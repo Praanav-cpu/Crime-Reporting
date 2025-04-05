85ffb69087aaf583856b0622e5fd716a142c0fdc
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-05 13:38:15 +0530

    folers and files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+    <div className="flex items-center justify-center min-h-screen bg-red-00">
       <h1 className="text-4xl font-bold text-blue-700"> write your Crime Here</h1>
     </div>
   );
